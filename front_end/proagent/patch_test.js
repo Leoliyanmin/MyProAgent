92cd2eeec993f8d5db887ad4d8c25acb8ab88cd3
Leoliyanmin <2733284963@qq.com>
2026-04-16 12:33:55 +0800

    chore: clean up repository and update .gitignore
    - Remove test files (test*.html, test*.js, patch_test.*)
    
    - Remove debug files (debug.html)
    
    - Remove temporary assets (image.png, paperwall_3.jpg)
    
    - Remove .DS_Store files
    
    - Move .gitignore to project root
    
    - Add rules for test files, debug files, and temporary assets

--- front_end/proagent/patch_test.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/stores/theme.js', 'utf8');
-code = code.replace(
-  "bgSidebarImage: '',",
-  "bgSidebarImage: 'data:image/svg+xml;utf8,<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"100\" height=\"100\"><rect width=\"100\" height=\"100\" fill=\"red\"/></svg>',"
-);
-fs.writeFileSync('src/stores/theme.js', code);
--- front_end/proagent/test3.js
+++
@@ -1,2 +0,0 @@
-const r = "data:image/png;base64,123";
-console.log(`url("${r}")`);
